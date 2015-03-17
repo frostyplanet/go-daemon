f3dd1dc8b7f32e0ced4b1531f0bd6cf89fb3eeed
plan <ningan@xunlei.com>
2015-03-17 14:17:13 +0800

    fix for long exe name

--- util_posix.go
+++ util_posix.go
@@ -1,15 +1,11 @@
 package daemon
 
 import (
-	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"strings"
-	"path"
-	)
+)
 
-//return 
+//return
 func GetExecPath(pid int) (string, error) {
 	proc_exe_link := fmt.Sprintf("/proc/%d/exe", pid)
 	link_target, err := os.Readlink(proc_exe_link)
@@ -19,27 +15,17 @@ func GetExecPath(pid int) (string, error) {
 	return link_target, nil
 }
 
-
 func IsProcessRunning(pid int) bool {
-    content, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/stat", pid))
-    if err != nil {
-        return false
-    }
-    scanner := bufio.NewScanner(strings.NewReader(string(content)))
-    scanner.Split(bufio.ScanWords)
-    scanner.Scan()
-    scanner.Scan()
-    if scanner.Err() != nil {
-        return false
-    }
-    pgname := scanner.Text()
-    pgname = pgname[1:len(pgname)-1]
-	real_path, err := GetExecPath(os.Getpid())
- 	if err != nil {
+	my_path, err := GetExecPath(os.Getpid())
+	if err != nil {
 		return false
 	}
-    if pgname == path.Base(real_path) {
-        return true
-    }
-    return false
+	exe_path, err := GetExecPath(pid)
+	if err != nil {
+		return false
+	}
+	if my_path == exe_path {
+		return true
+	}
+	return false
 }
